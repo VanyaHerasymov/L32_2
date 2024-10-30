f7cf2c104e2b7f6747394cbcbbb28ea3c618f538
HerasymovIvan <gerasymov.ivan@gmail.com>
2024-10-30 18:22:46 +0200

    Header File

--- L32_2/Product.cpp
+++ L32_2/Product.cpp
@@ -1,1 +1,49 @@
 #include "Product.h"
+
+Products::Product::Product()
+{
+}
+
+Products::Product::Product(std::string name, float price, std::string manufacturer, int age)
+{
+}
+
+Products::Product::~Product()
+{
+}
+
+void Products::Product::setName(std::string name)
+{
+}
+
+std::string Products::Product::getName() const
+{
+    return std::string();
+}
+
+void Products::Product::setPrice(float price)
+{
+}
+
+float Products::Product::getPrice() const
+{
+    return 0.0f;
+}
+
+void Products::Product::setManufacturer(std::string manufacturer)
+{
+}
+
+std::string Products::Product::getManufacturer() const
+{
+    return std::string();
+}
+
+void Products::Product::setAge(int age)
+{
+}
+
+int Products::Product::getAge() const
+{
+    return 0;
+}
--- L32_2/Product.h
+++ L32_2/Product.h
@@ -1,5 +1,44 @@
 #pragma once
-class Product
-{
-};
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+
+
+
+namespace Products {
+	class Product
+	{
+	protected:
+		std::string name;
+		float price;
+		std::string manufacturer;
+		int age;
+	public:
+		Product();
+		Product(std::string name, float price, std::string manufacturer, int age);
+		virtual ~Product();
+		void setName(std::string name);
+		std::string getName()const;
+
+		void setPrice(float price);
+		float getPrice()const;
+
+
+		void setManufacturer(std::string manufacturer);
+		std::string getManufacturer()const;
+
+		void setAge(int age);
+		int getAge()const;
+
+		virtual void show()const = 0;
+		virtual float calcPrice()const = 0;
+
+
+	};
+
+
+}
+
+
 
