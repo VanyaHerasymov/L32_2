d190dc44df5ee1fa7e11b64be7b53a108766aff3
HerasymovIvan <gerasymov.ivan@gmail.com>
2024-10-30 18:26:50 +0200

    Definition

--- L32_2/Product.cpp
+++ L32_2/Product.cpp
@@ -2,48 +2,65 @@
 
 Products::Product::Product()
 {
+    name = "no name";
+    price = 0;
+    manufacturer = "no name";
+    age = 0;
 }
 
 Products::Product::Product(std::string name, float price, std::string manufacturer, int age)
 {
+    this->name = name;
+    this->price = price;
+    this->manufacturer = manufacturer;
+    this->age = age;
 }
 
 Products::Product::~Product()
 {
+
 }
 
 void Products::Product::setName(std::string name)
 {
+    this->name = name;
+
 }
 
 std::string Products::Product::getName() const
 {
-    return std::string();
+    return name;
 }
 
 void Products::Product::setPrice(float price)
 {
+    this->price = price;
+
 }
 
 float Products::Product::getPrice() const
 {
-    return 0.0f;
+    return price;
 }
 
 void Products::Product::setManufacturer(std::string manufacturer)
 {
+    this->manufacturer = manufacturer;
+
 }
 
 std::string Products::Product::getManufacturer() const
 {
-    return std::string();
+    return manufacturer;
 }
 
 void Products::Product::setAge(int age)
 {
+    this->age = age;
+
 }
 
 int Products::Product::getAge() const
 {
-    return 0;
+    return age;
 }
